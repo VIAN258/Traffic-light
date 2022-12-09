8513362a160f5f124f263708ad9b172b376e3dcb
VIAN258 <angelmorales891@gmail.com>
2022-12-09 00:21:38 +0000

    actualizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,11 +6,20 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const[color, setColor] = useState("red")
+	const change = (color) => setColor(previous => color)
+
+
 	return (
 	<body>
 		<div class="semaforo">
 			<div class='primero'>
-				<div class="rojo"></div>
+				{
+                  color == red ? <div class="rojo red"></div> : <div class="rojo"></div>
+				  
+				}
+				<a onClick={() => change("red")} href="#"></a>
+				
 			</div>
 			<div class='segundo'>
 				<div class="amarillo"></div>

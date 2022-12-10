8c1ee20fd8b7e4f741e75ed9c6cd76d9c1f8f3f8
VIAN258 <angelmorales891@gmail.com>
2022-12-10 02:42:22 +0000

    3erupda

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,30 +6,22 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const[selected, setSelected] = useState("")
-	const modified = color => {
-		if (color === selected) {
-			setSelected("");
-		} else {
-			setSelected(color);
-		}
-	};
+	const[selected, setSelected] = useState("rojo")
+	
+	const change = (selected) => setSelected(previous => selected)
 
 	return (
 	<body>
 		<div className="semaforo">
 			<div className='primero'>
 		
-                  <div className={
-					selected == "red" ? "rojo red" : "rojo"
-				  }
-				    onClick={() => modified("red")}></div>
-				
+                <div className={selected == "rojo" ? "rojo red" : "rojo"}  onClick={() => change("rojo")}></div>
+					
 			</div>
 			<div className='segundo'>
 				<div className="amarillo"></div>
 			</div>
-			<div className='segundo'>
+			<div className='tercero'>
 				<div className="verde"></div>
 			</div>
 		</div>
